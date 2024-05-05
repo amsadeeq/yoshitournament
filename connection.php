9f9125b20b8c47498ea1749346d9970035fe96f6
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-05-06 03:55:01 +0800

    Added Connection to Hostinger Database

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;dbname=yoshi_tournament_db', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=u176084735_yoshi_db', 'u176084735_uaeyoshi', 'L3t$Cr3@t3');
 
 ?>
