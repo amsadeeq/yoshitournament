756379194cb5b44eff437b312efa97b082f38871
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-05-27 01:51:30 +0800

    Tournamnet

--- about.php
+++ about.php
@@ -182,6 +182,41 @@
 
     </div>
 
+    <div class="about-page-main comon-sub-page-main">
+
+      <div class="about-club-top">
+        <div class="container">
+          <div class="row row-cols-1 row-cols-lg-2 g-lg-5">
+            <div class="col">
+              <h5 class="samll-sub mb-1 mt-0"> The Story </h5>
+              <h2 class="comon-heading m-0"> About Yoshi FA World Wide Tournament </h2>
+              <p class="mt-3"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also
+                the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                with the release of Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is
+                a long established fact that a
+                reader will be distracted by the readable content of a page when looking at its layout. The point of
+                using Lorem Ipsum
+              </p>
+
+            </div>
+            <div class="col position-relative">
+              <figure class="big-img">
+                <img src="images/Yoshi_10.jpg" alt="pic">
+              </figure>
+              <figure class="small-img">
+                <img src="images/pexels-photo-12460951.jfif" alt="pic2">
+              </figure>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+
     <div class="our-history-div">
 
 
