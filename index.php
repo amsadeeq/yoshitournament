73daa2b9c73ffe4d9e36661f0b9341d4c3d1bbc4
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-05-26 23:00:11 +0800

    button

--- index.php
+++ index.php
@@ -664,7 +664,7 @@ if (isset($_POST['register'])) {
               </h2>
               <p> Football tournaments are not just about winning games; they're about the passion, camaraderie, and
                 unforgettable moments that unite players and fans alike in the beautiful game</p>
-              <button type="button" class="buy-now-btn" name="button">
+              <a href="about.php" class="buy-now-btn" name="button">
 
                 Tournament
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -672,7 +672,7 @@ if (isset($_POST['register'])) {
                   <path fill-rule="evenodd"
                     d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z">
                   </path>
-                </svg> </button>
+                </svg> </a>
 
 
 
