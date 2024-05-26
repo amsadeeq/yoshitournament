77c596044c25d7cc61f6f9d78c0491035c26305a
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-05-27 01:25:12 +0800

    News

--- media.php
+++ media.php
@@ -60,24 +60,24 @@
               <a class="nav-link" href="matches.php">Events</a>
             </li>
 
-            <li class="nav-item">
-              <a class="nav-link " href="schedule.php">Schedule</a>
-            </li>
+            <!-- <li class="nav-item">
+          <a class="nav-link " href="schedule.php">Schedule</a>
+        </li> -->
 
             <li class="nav-item">
               <a class="nav-link " href="news.php">News</a>
             </li>
 
-            <li class="nav-item">
-              <a class="nav-link " href="players.php">Players</a>
-            </li>
+            <!-- <li class="nav-item">
+          <a class="nav-link " href="players.php">Players</a>
+        </li> -->
 
             <li class="nav-item">
               <a class="nav-link active" href="media.php">Media</a>
             </li>
             <!-- <li class="nav-item">
-        <a class="nav-link " href="shop.php">Shop</a>
-      </li> -->
+            <a class="nav-link " href="shop.php">Shop</a>
+          </li> -->
 
             <li class="nav-item">
               <a class="nav-link " href="contact.php">Contact</a>
