010de50065d81e4b4b80b7c361b29df7f8b8b63a
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-06-02 21:49:28 +0800

    contact update

--- contact.php
+++ contact.php
@@ -116,7 +116,7 @@
         <h1 class="text-center"> Contact</h1>
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
             <li class="breadcrumb-item active" aria-current="page">Contact</li>
           </ol>
         </nav>
