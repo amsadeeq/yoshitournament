080fb7cbeedaf0346a8dc93fd679d3461574b187
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-06-02 21:47:37 +0800

    navbar

--- matches-details.php
+++ matches-details.php
@@ -52,11 +52,11 @@
 
 
             <li class="nav-item">
-              <a class="nav-link active" href="about.php">Tournament</a>
+              <a class="nav-link" href="about.php">Tournament</a>
             </li>
 
             <li class="nav-item">
-              <a class="nav-link" href="matches.php">Events</a>
+              <a class="nav-link active" href="matches.php">Events</a>
             </li>
 
             <li class="nav-item">
