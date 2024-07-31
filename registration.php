32344aa150268af622c8d3f17e866b7a7e4a3d6d
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-01 02:49:19 +0800

    regCard

--- registration.php
+++ registration.php
@@ -160,7 +160,7 @@
             <div class="row g-0 align-items-center">
                 <!-- Repeat the card for the remaining 3 columns -->
                 <div class="col-sm-6 col-xs-6">
-                    <div class="card rounded-5 shadow-lg" style="border-radius:10px 10px;">
+                    <div class="card rounded-5 shadow-lg" style=" cursor:pointer; border-radius:10px 10px;">
                         <div class="row g-0 align-items-center">
                             <div class="col-xs-6 col-sm-6">
                                 <img src="images/Yoshi Modal.png" class="card-img-top" alt="Gift Poster"
