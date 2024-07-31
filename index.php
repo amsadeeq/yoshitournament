3cccc978c1b1152e79eb2fad3418c367a961ec9c
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-01 03:21:31 +0800

    index changes

--- index.php
+++ index.php
@@ -1880,7 +1880,7 @@ if (isset($_POST['register'])) {
                         aria-label="Close"></button>
                       <div class="px-4 animated-text">
                         <h2>Welcome to!</h2>
-                        <h2>Yoshi Abuja Private Schools Torunaments 2024</h2>
+                        <h2>Yoshi Abuja Private Schools Tournaments 2024</h2>
                         <!-- <p class="my-3 text-secondary">Yoshi Abuja Private Schools Torunaments 2024</p>
                         <p class="mt-3 mb-4 text-success">Football Tournament</p> -->
 
