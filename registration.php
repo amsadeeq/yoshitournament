8a238c16529937fa695d437eb67b0dc553daeeba
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-01 02:37:02 +0800

    modal

--- registration.php
+++ registration.php
@@ -159,26 +159,30 @@
         <div class="container matech-div-main my-5">
             <div class="row g-0 align-items-center">
                 <!-- Repeat the card for the remaining 3 columns -->
-                <div class="col">
+                <div class="col-sm-6 col-xs-6">
                     <div class="card">
-                        <div class="col-xs-6 col-lg-6 col-sm-6">
-                            <img src="images/Yoshi Modal.png" class="card-img-top" alt="Gift Poster"
-                                style="border-radius: 10px 10px;">
-                        </div>
-                        <div class="col-xs-6 col-lg-6 col-sm-6">
-                            <div class="card-body text-center">
-                                <div class="px-4 animated-text">
-                                    <h2>Welcome to!</h2>
-                                    <p class="my-3 text-secondary">Yoshi Abuja Private Schools Tournaments 2024</p>
-                                    <p class="mt-3 mb-4 text-success">Football Tournament</p>
-                                    <a href="registration.php" class="btn mt-8 w-100 join-btn"
-                                        style="border-radius: 15px 15px;">Register</a>
+                        <div class="row g-0 align-items-center">
+                            <div class="col-xs-6 col-sm-6">
+                                <img src="images/Yoshi Modal.png" class="card-img-top" alt="Gift Poster"
+                                    style="border-radius: 10px 10px;">
+                            </div>
+                            <div class="col-xs-6 col-lg-6 col-sm-6">
+                                <div class="card-body text-center">
+                                    <div class="px-4 animated-text">
+                                        <h2>Welcome to!</h2>
+                                        <p class="my-3 text-secondary">Yoshi Abuja Private Schools Tournaments 2024</p>
+                                        <p class="mt-3 mb-4 text-success">Football Tournament</p>
+                                        <a href="registration.php" class="btn mt-8 w-100 join-btn"
+                                            style="border-radius: 15px 15px;">Register</a>
+                                    </div>
+                                    <a href="#"
+                                        class="text-secondary mt-3 text-decoration-none animated-text"><small>Login
+                                            to
+                                            continue</small></a>
                                 </div>
-                                <a href="#" class="text-secondary mt-3 text-decoration-none animated-text"><small>Login
-                                        to
-                                        continue</small></a>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
