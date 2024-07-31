52f0dab6e79da0474af2fbfff8f32ad14eaf88c0
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-01 03:27:14 +0800

    regCard index

--- index.php
+++ index.php
@@ -1880,11 +1880,12 @@ if (isset($_POST['register'])) {
                         aria-label="Close"></button>
                       <div class="px-4 animated-text">
                         <h2>Welcome to!</h2>
+                        <br /><br />
                         <h1>Yoshi Abuja Private Schools Tournaments 2024</h1>
                         <!-- <p class="my-3 text-secondary">Yoshi Abuja Private Schools Torunaments 2024</p>
                         <p class="mt-3 mb-4 text-success">Football Tournament</p> -->
 
-                        <br /><br /><br /><br /><br />
+                        <br /><br /><br />
 
                         <a type="button" href="registration.php" class="btn mt-8 w-100 join-btn"
                           style="border-radius: 15px 15px;">Register</a>
