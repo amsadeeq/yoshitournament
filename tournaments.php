b9e32d06f3eb16d83cfe143ba5697d74e4da6eed
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-06 22:28:13 +0800

    Refactor tournaments.php to add card for Yoshi Abuja Private School Tournament 2024

--- tournaments.php
+++ tournaments.php
@@ -157,6 +157,43 @@
 
     <section class="body-part-total float-start w-100">
 
+        <div class="container matech-div-main my-5">
+            <div class="row g-4 align-items-center">
+                <!-- Repeat the card for the remaining 2 columns -->
+                <div class="col-sm-6 col-xs-6">
+                    <div class="card rounded-5 shadow-lg animate-onload" data-bs-toggle="modal"
+                        data-bs-target="#loginModal" style="cursor:pointer; border-radius:10px 10px;">
+                        <div class="row g-0 align-items-center">
+                            <div class="col-xs-6 col-sm-6">
+                                <img src="images/yoshiStud.png" class="card-img-top d-none d-sm-block" alt="Gift Poster"
+                                    style="border-radius: 10px 0px 0px 10px;">
+                                <img src="images/yoshiStud.png" class="card-img-top d-sm-none" alt="Gift Poster"
+                                    style="border-radius: 10px 10px 10px 10px;">
+                            </div>
+                            <div class="col-xs-6 col-lg-6 col-sm-6">
+                                <div class="card-body text-center">
+                                    <div class="px-4 animated-text">
+                                        <h2>Yoshi Abuja Private School Tournament 2024</h2>
+                                        <br />
+                                        <!-- <p class="my-3 text-secondary">Yoshi Abuja Private Schools Tournaments 2024</p> -->
+                                        <!-- <p class="mt-3 mb-4 text-success">Football Tournament</p> -->
+                                        <a data-bs-toggle="modal" data-bs-target="#loginModal"
+                                            class="btn mt-8 w-100 join-btn shadow-sm"
+                                            style="border-radius: 15px 15px;">Register</a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
         <div class="about-page-main comon-sub-page-main">
 
             <div class="about-club-top">
