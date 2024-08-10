544d930f6168c482322959a2553762e43ca435a8
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-10 12:58:23 +0800

    chore: Remove unnecessary code and update navigation in school_registration.php

--- school_registration.php
+++ school_registration.php
@@ -376,69 +376,7 @@ if (isset($_POST["complete_register"])) {
 
 <body>
 
-  <header class="float-start w-100">
-
-    <nav class="navbar navbar-expand-lg navbar-light ">
-      <div class="container">
-        <a class="navbar-brand" href="index.php">
-          <img src="images/logo.png" alt="logo" class="yoshi_logo" />
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRightmobile">
-          <i class="fas fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
-            <!-- <li class="nav-item">
-               <a class="nav-link" href="matches.php">Matches</a>
-             </li> -->
-
-            <!-- <li class="nav-item">
-               <a class="nav-link " href="about.php">The Tournament</a>
-             </li> -->
-
-            <!-- <li class="nav-item">
-                <a class="nav-link " href="schedule.php">Schedule</a>
-              </li> -->
-
-            <!-- <li class="nav-item">
-                <a class="nav-link " href="news.php">News</a>
-              </li> -->
-
-            <!-- <li class="nav-item">
-                <a class="nav-link " href="players.php">Players</a>
-              </li> -->
-
-            <!-- <li class="nav-item">
-                <a class="nav-link" href="media.php">Media</a>
-              </li> -->
-
-            <!-- <li class="nav-item">
-                <a class="nav-link " href="shop.php">Shop</a>
-              </li> -->
-
-            <!-- <li class="nav-item">
-                <a class="nav-link " href="contact.php">Contact</a>
-              </li> -->
-
-            <!-- <li class="nav-item">
-               <a class="nav-link btn join-btn" data-bs-toggle="modal" class="regster-bn" data-bs-target="#loginModal">
-                 
-                 Register</a>
-             </li> -->
-
-            <li class="nav-item">
-              <a class="nav-link btn bar-btn" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRightmobile"><i
-                  class="fas fa-bars"></i></a>
-            </li>
-
-          </ul>
-
-        </div>
-      </div>
-    </nav>
-
-  </header>
+  
 
   <section class="sub-main-banner float-start w-100">
 
