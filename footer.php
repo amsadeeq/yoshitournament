05f0bdad1ad23e71d2dbcc087ec0824e0580af03
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-11 00:33:31 +0800

    Refactor image URLs in tournaments.php and partners.php

--- footer.php
+++ footer.php
@@ -79,7 +79,7 @@ echo "
 
                 </div>
                 <hr/>
-                <p class='text-center copy-t'> Copyright 2024 <a style = 'text-decoration: none;' target = '_blank' href = 'https://yoshifa.com'>Yoshi Football Academy</a>, All Right Reserved</p>
+                <p class='text-center copy-t'> Copyright 2024 <a style = 'text-decoration: none;' target = '_blank' href = 'https://yoshifa.com'>Yoshi Football Academy UAE</a>, All Right Reserved</p>
         </div>
         </footer>
     ";
