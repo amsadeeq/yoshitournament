8f0e8dceef6f65d54a434a76e6f7b2415ca61ddc
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-11 18:30:00 +0800

    Refactor registration pages to use shared header file

--- official_registration.php
+++ official_registration.php
@@ -367,70 +367,7 @@ if (isset($_POST["complete_register"])) {
 
 <body>
 
-    <header class="float-start w-100">
-
-        <nav class="navbar navbar-expand-lg navbar-light ">
-            <div class="container">
-                <a class="navbar-brand" href="index.php">
-                    <img src="images/logo.png" alt="logo" class="yoshi_logo" />
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasRightmobile">
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
-                        <!-- <li class="nav-item">
-               <a class="nav-link" href="matches.php">Matches</a>
-             </li> -->
-
-                        <!-- <li class="nav-item">
-               <a class="nav-link " href="about.php">The Tournament</a>
-             </li> -->
-
-                        <!-- <li class="nav-item">
-                <a class="nav-link " href="schedule.php">Schedule</a>
-              </li> -->
-
-                        <!-- <li class="nav-item">
-                <a class="nav-link " href="news.php">News</a>
-              </li> -->
-
-                        <!-- <li class="nav-item">
-                <a class="nav-link " href="players.php">Players</a>
-              </li> -->
-
-                        <!-- <li class="nav-item">
-                <a class="nav-link" href="media.php">Media</a>
-              </li> -->
-
-                        <!-- <li class="nav-item">
-                <a class="nav-link " href="shop.php">Shop</a>
-              </li> -->
-
-                        <!-- <li class="nav-item">
-                <a class="nav-link " href="contact.php">Contact</a>
-              </li> -->
-
-                        <!-- <li class="nav-item">
-               <a class="nav-link btn join-btn" data-bs-toggle="modal" class="regster-bn" data-bs-target="#loginModal">
-                 
-                 Register</a>
-             </li> -->
-
-                        <li class="nav-item">
-                            <a class="nav-link btn bar-btn" data-bs-toggle="offcanvas"
-                                data-bs-target="#offcanvasRightmobile"><i class="fas fa-bars"></i></a>
-                        </li>
-
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
-
-    </header>
+    <?php require 'reg_header.php'; ?>
 
     <section class="sub-main-banner float-start w-100">
 
@@ -440,7 +377,7 @@ if (isset($_POST["complete_register"])) {
                 <h1 class="text-center"> Official Registration </h1>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
                         <li class="breadcrumb-item active" aria-current="page"> Registration </li>
                     </ol>
                 </nav>
