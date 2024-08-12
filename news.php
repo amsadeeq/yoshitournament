cc0620e653b32ac1bb76b28994d47537dd0c5096
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-12 21:16:04 +0800

    Refactor news.php to update heading and improve page load with lazy loading for images

--- news.php
+++ news.php
@@ -128,15 +128,15 @@ require 'auth.php';
                   </li>
                 </ul>
 
-                <p class="mt-2"> Ahmad's journey from a football-loving child in Africa to a certified coach and
-                  entrepreneur in the UAE is a testament to the power of resilience and determination </p>
+                <p class="mt-2" style="text-align: justify !important;"> Ahmad's journey from a football-loving child in
+                  Africa to a certified coach and entrepreneur in the UAE is a testament to the power of resilience and
+                  determination </p>
 
-                <a href="https://www.khaleejtimes.com/kt-network/turning-dreams-into-reality-ahmad-mustapha-geidams-soccer-triumph-with-yoshi-academy"
-                  class="btn reda-btn mt-3"> Read more <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                <h6 class="btn reda-btn mt-3"> Read more <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                     fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
                     <path fill-rule="evenodd"
                       d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z" />
-                  </svg> </a>
+                  </svg> </h6>
               </div>
 
             </a>
