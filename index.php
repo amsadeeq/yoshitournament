dd08e5ae7a9b3ffdc4410964e83d907557d96b45
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-16 02:30:44 +0800

    Refactor tournament category headings and next match information in index.php

--- index.php
+++ index.php
@@ -223,9 +223,9 @@ require 'auth.php';
           <hr class="next_match_line" />
           <h4> YAPS TOURNAMENT CATEGORIES 2024</h4>
 
-          <p class=" text-white">Primary School - 14th Sept 2024</p>
-          <p class=" text-white">Junior Sec School - 21st Sept 2024</p>
-          <p class=" text-white">Senior Sec School - 28th Sept 2024</p>
+          <p class=" text-white text-light">Primary School - 14th Sept 2024</p>
+          <p class=" text-white text-light">Junior Sec School - 21st Sept 2024</p>
+          <p class=" text-white text-light">Senior Sec School - 28th Sept 2024</p>
 
 
           <!-- <div class="d-flex align-items-center justify-content-center mt-2">
@@ -330,183 +330,9 @@ require 'auth.php';
       </div>
     </div>
 
-    <div class="result-div1 mt-5">
-      <div class="container">
-
-        <div class="row gx-lg-5">
-          <div class="col-lg-7 col-xl-8">
-            <div class="d-flex justify-content-between align-items-center">
-              <h2 class="comon-heading m-0">Matches</h2>
-              <a href="matches.php" class="btn all-cm-btn  animate__animated animate__zoomIn"> All Matches <svg
-                  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                  class="bi bi-arrow-right" viewBox="0 0 16 16">
-                  <path fill-rule="evenodd"
-                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                </svg> </a>
-            </div>
-            <div class="row row-cols-1 row-cols-lg-2 gy-5 g-lg-4 mt-0 mt-lg-0">
-              <div class="col  animate__animated animate__zoomIn">
-                <a href="#" class="items-matchs">
-                  <figure class="m-0 bg-mc-1">
-                    <img src="images/bg-mc.jpg" alt="bg-ms" />
-                  </figure>
-                  <div class="matches-div-home">
-                    <h5> Next Match
-                      <span class="d-block"> Coming Soon! </span>
-                    </h5>
-                    <!-- <h5> Pantami Stadium, Gombe
-                      <span class="d-block"> June 02, 2024 </span>
-                    </h5>
-                    <div class="d-flex align-items-center justify-content-between">
-
-                      <figure>
-                        <img src="images/team_3.png" alt="cl2" />
-                        <figcaption>Kano Pillars </figcaption>
-                      </figure>
-                      <h4>VS</h4>
-                      <figure>
-                        <img src="images/team_4.png" alt="cl2" />
-                        <figcaption>Yobe Untd</figcaption>
-                      </figure>
-                    </div> -->
-                  </div>
-                </a>
-              </div>
-
-              <div class="col  animate__animated animate__zoomIn">
-                <a href="#" class="items-matchs">
-                  <figure class="m-0 bg-mc-1">
-                    <img src="images/bg-ms2.jpeg" alt="bg-ms" />
-                  </figure>
-                  <div class="matches-div-home">
-                    <h5> Next Match
-                      <span class="d-block"> Coming Soon! </span>
-                    </h5>
-                    <!-- <h5> Abuja Central Stadium
-                      <span class="d-block"> June 03, 2024 </span>
-                    </h5>
-                    <div class="d-flex align-items-center justify-content-between">
-
-                      <figure>
-                        <img src="images/team_5.png" alt="cl2" />
-                        <figcaption>Lobi Stars </figcaption>
-                      </figure>
-                      <h4>VS</h4>
-                      <figure>
-                        <img src="images/team_7.png" alt="cl2" />
-                        <figcaption>Jos Untd</figcaption>
-                      </figure>
-                    </div> -->
-                  </div>
-                </a>
-              </div>
-
-              <div class="col  animate__animated animate__zoomIn">
-                <a href="#" class="items-matchs">
-                  <figure class="m-0 bg-mc-1">
-                    <img src="images/bg-mc3.jpeg" alt="bg-ms" />
-                  </figure>
-                  <div class="matches-div-home">
-                    <h5> Next Match
-                      <span class="d-block"> Coming Soon! </span>
-                    </h5>
-                    <!-- <h5> Abuja Central Stadium
-                      <span class="d-block"> June 03, 2024 </span>
-                    </h5>
-                    <div class="d-flex align-items-center justify-content-between">
-
-                      <figure>
-                        <img src="images/team_5.png" alt="cl2" />
-                        <figcaption>DC Unfo.</figcaption>
-                      </figure>
-                      <h4>VS</h4>
-                      <figure>
-                        <img src="images/team_2.jpg" alt="cl2" />
-                        <figcaption>Italy FC.</figcaption>
-                      </figure>
-                    </div> -->
-                  </div>
-                </a>
-              </div>
+    <!--- ####### Matches and Result will be here <?php require 'matchesResult.php'; ?> ########## --->
 
 
-              <div class="col  animate__animated animate__zoomIn">
-                <a href="#" class="items-matchs">
-                  <figure class="m-0 bg-mc-1">
-                    <img src="images/bg-mc.jpg" alt="bg-ms" />
-                  </figure>
-                  <div class="matches-div-home">
-                    <h5> Next Match
-                      <span class="d-block"> Coming Soon! </span>
-                    </h5>
-                    <!-- <h5> Yobe Central Stadium
-                      <span class="d-block"> June 03, 2024 </span>
-                    </h5>
-                    <div class="d-flex align-items-center justify-content-between">
-
-                      <figure>
-                        <img src="images/team_3.png" alt="cl2" />
-                        <figcaption>Kano P.</figcaption>
-                      </figure>
-                      <h4>VS</h4>
-                      <figure>
-                        <img src="images/team_5.png" alt="cl2" />
-                        <figcaption>Italy FC.</figcaption>
-                      </figure>
-                    </div> -->
-                  </div>
-                </a>
-              </div>
-
-            </div>
-
-          </div>
-          <div class="col-lg-5 col-xl-4 ">
-            <div class="latest-result-div mt-5 mt-lg-0">
-              <div class="d-flex align-items-center justify-content-between">
-                <h2 class="comon-heading m-0"> Results</h2>
-                <a href="matches.php" class="btn viw-btn">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                    class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
-                    <path
-                      d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z" />
-                  </svg>
-                </a>
-              </div>
-
-              <div class="ltest-divbm mt-4">
-                <a href="#" class="comon-items-div">
-                  <div class="d-flex justify-content-between align-items-center">
-                    <h5> Match Results
-                      <span class="d-block"> Updating... </span>
-                    </h5>
-
-                    <!-- <div class="cm-culb">
-                      <figure class="d-flex align-items-center">
-                        <figcaption class="me-2">Gombe Utd</figcaption>
-                        <img src="images/fc-1.jpg" alt="cl2" />
-
-                      </figure>
-
-                    </div>
-                    <h5 class="m-0"> 1 - 2 </h5>
-                    <div class="cm-culb">
-                      <figure class="d-flex align-items-center">
-                        <figcaption class="me-2">Abuja FC.</figcaption>
-                        <img src="images/fc-2.jpg" alt="cl2" />
-
-                      </figure>
-
-                    </div> -->
-                  </div>
-                  <!-- <p class="text-center"> <i class="fas fa-map-marker-alt"></i> Dec 2, 2023/ Abuja Stadium</p> -->
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
 
     <div class="home-about-part">
       <div class="container">
---
+++ matchesResutl.php
@@ -0,0 +1,177 @@
+<div class="result-div1 mt-5">
+    <div class="container">
+
+        <div class="row gx-lg-5">
+            <div class="col-lg-7 col-xl-8">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h2 class="comon-heading m-0">Matches</h2>
+                    <a href="matches.php" class="btn all-cm-btn  animate__animated animate__zoomIn"> All Matches <svg
+                            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                        </svg> </a>
+                </div>
+                <div class="row row-cols-1 row-cols-lg-2 gy-5 g-lg-4 mt-0 mt-lg-0">
+                    <div class="col  animate__animated animate__zoomIn">
+                        <a href="#" class="items-matchs">
+                            <figure class="m-0 bg-mc-1">
+                                <img src="images/bg-mc.jpg" alt="bg-ms" />
+                            </figure>
+                            <div class="matches-div-home">
+                                <h5> Next Match
+                                    <span class="d-block"> Coming Soon! </span>
+                                </h5>
+                                <!-- <h5> Pantami Stadium, Gombe
+                      <span class="d-block"> June 02, 2024 </span>
+                    </h5>
+                    <div class="d-flex align-items-center justify-content-between">
+
+                      <figure>
+                        <img src="images/team_3.png" alt="cl2" />
+                        <figcaption>Kano Pillars </figcaption>
+                      </figure>
+                      <h4>VS</h4>
+                      <figure>
+                        <img src="images/team_4.png" alt="cl2" />
+                        <figcaption>Yobe Untd</figcaption>
+                      </figure>
+                    </div> -->
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col  animate__animated animate__zoomIn">
+                        <a href="#" class="items-matchs">
+                            <figure class="m-0 bg-mc-1">
+                                <img src="images/bg-ms2.jpeg" alt="bg-ms" />
+                            </figure>
+                            <div class="matches-div-home">
+                                <h5> Next Match
+                                    <span class="d-block"> Coming Soon! </span>
+                                </h5>
+                                <!-- <h5> Abuja Central Stadium
+                      <span class="d-block"> June 03, 2024 </span>
+                    </h5>
+                    <div class="d-flex align-items-center justify-content-between">
+
+                      <figure>
+                        <img src="images/team_5.png" alt="cl2" />
+                        <figcaption>Lobi Stars </figcaption>
+                      </figure>
+                      <h4>VS</h4>
+                      <figure>
+                        <img src="images/team_7.png" alt="cl2" />
+                        <figcaption>Jos Untd</figcaption>
+                      </figure>
+                    </div> -->
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col  animate__animated animate__zoomIn">
+                        <a href="#" class="items-matchs">
+                            <figure class="m-0 bg-mc-1">
+                                <img src="images/bg-mc3.jpeg" alt="bg-ms" />
+                            </figure>
+                            <div class="matches-div-home">
+                                <h5> Next Match
+                                    <span class="d-block"> Coming Soon! </span>
+                                </h5>
+                                <!-- <h5> Abuja Central Stadium
+                      <span class="d-block"> June 03, 2024 </span>
+                    </h5>
+                    <div class="d-flex align-items-center justify-content-between">
+
+                      <figure>
+                        <img src="images/team_5.png" alt="cl2" />
+                        <figcaption>DC Unfo.</figcaption>
+                      </figure>
+                      <h4>VS</h4>
+                      <figure>
+                        <img src="images/team_2.jpg" alt="cl2" />
+                        <figcaption>Italy FC.</figcaption>
+                      </figure>
+                    </div> -->
+                            </div>
+                        </a>
+                    </div>
+
+
+                    <div class="col  animate__animated animate__zoomIn">
+                        <a href="#" class="items-matchs">
+                            <figure class="m-0 bg-mc-1">
+                                <img src="images/bg-mc.jpg" alt="bg-ms" />
+                            </figure>
+                            <div class="matches-div-home">
+                                <h5> Next Match
+                                    <span class="d-block"> Coming Soon! </span>
+                                </h5>
+                                <!-- <h5> Yobe Central Stadium
+                      <span class="d-block"> June 03, 2024 </span>
+                    </h5>
+                    <div class="d-flex align-items-center justify-content-between">
+
+                      <figure>
+                        <img src="images/team_3.png" alt="cl2" />
+                        <figcaption>Kano P.</figcaption>
+                      </figure>
+                      <h4>VS</h4>
+                      <figure>
+                        <img src="images/team_5.png" alt="cl2" />
+                        <figcaption>Italy FC.</figcaption>
+                      </figure>
+                    </div> -->
+                            </div>
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col-lg-5 col-xl-4 ">
+                <div class="latest-result-div mt-5 mt-lg-0">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <h2 class="comon-heading m-0"> Results</h2>
+                        <a href="matches.php" class="btn viw-btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z" />
+                            </svg>
+                        </a>
+                    </div>
+
+                    <div class="ltest-divbm mt-4">
+                        <a href="#" class="comon-items-div">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h5> Match Results
+                                    <span class="d-block"> Updating... </span>
+                                </h5>
+
+                                <!-- <div class="cm-culb">
+                      <figure class="d-flex align-items-center">
+                        <figcaption class="me-2">Gombe Utd</figcaption>
+                        <img src="images/fc-1.jpg" alt="cl2" />
+
+                      </figure>
+
+                    </div>
+                    <h5 class="m-0"> 1 - 2 </h5>
+                    <div class="cm-culb">
+                      <figure class="d-flex align-items-center">
+                        <figcaption class="me-2">Abuja FC.</figcaption>
+                        <img src="images/fc-2.jpg" alt="cl2" />
+
+                      </figure>
+
+                    </div> -->
+                            </div>
+                            <!-- <p class="text-center"> <i class="fas fa-map-marker-alt"></i> Dec 2, 2023/ Abuja Stadium</p> -->
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
