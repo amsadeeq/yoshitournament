9f355eed179876f57538f9a6685cff78325d9740
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-16 23:14:52 +0800

    Refactor button color in admin login page

--- management/admin/index.php
+++ management/admin/index.php
@@ -41,7 +41,7 @@
               <input type="password" class="form-control" name="a_password" placeholder="Password" required />
             </div>
             <div>
-              <button type="submit" class="btn btn-default submit">Continue</button>
+              <button type="submit" class="btn btn-warning submit">Continue</button>
 
               <a class="reset_pass" href="#">Lost your password?</a>
             </div>
