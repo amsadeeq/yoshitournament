857fef3385158e5db0d87b983d76b6c6708f4150
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-18 21:14:24 +0800

    Refactor yaps2024_modal.php: update modal size for better user experience

--- index.php
+++ index.php
@@ -334,7 +334,7 @@ require 'auth.php';
 
 
 
-    <div class="home-about-part">
+    <div class="home-about-part mt-0">
       <div class="container">
         <h5> Our History </h5>
         <h2 class="comon-heading m-0"> About the
@@ -358,190 +358,16 @@ require 'auth.php';
       </div>
     </div>
 
-    <!-- <div class="table-ag-div py-5">
-      <div class="container">
-        <h2 class="comon-heading m-0"> League Table & Schedule </h2>
+    <!--- ########## League Table & Schedules ########## --->
 
-        <div class="row g-lg-5">
-          <div class="col-lg-5">
-            <div class="table-div-left mt-4">
-              <h4> Batch A </h4>
+    <?php //require 'leagueSchedule.php' ?>
 
 
-              <table id="seri1" class="display" style="width:100%">
-                <thead>
-                  <tr>
-                    <th>Club</th>
-                    <th>W</th>
-                    <th>D</th>
-                    <th>L</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
 
-                      <div class="comon-ft-cl">
-                        <figure class="d-flex align-items-center">
-                          <img src="images/fc-1.jpg" alt="fbn" />
-                          <figcaption>
-                            &nbsp;Loading...
-                          </figcaption>
-                        </figure>
-                      </div>
+    <!--- ########## Player Squad ########## --->
 
-                    </td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                  </tr>
-                </tbody>
-                <tfoot>
-                  <tr>
-                    <th>Club</th>
-                    <th>W</th>
-                    <th>D</th>
-                    <th>L</th>
-                  </tr>
-                </tfoot>
-              </table>
+    <?php //require 'playerSquad.php' ?>
 
-            </div>
-          </div>
-          <div class="col-lg-7">
-            <div class="table-div-left mt-4">
-              <h4> Batch A </h4>
-
-
-              <table id="seri2" class="display " style="width:100%">
-                <thead>
-                  <tr>
-                    <th>Date</th>
-                    <th>Event</th>
-                    <th>Time</th>
-                    <th>Venue</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      0
-                    </td>
-                    <td> Loading.. <span>vs</span> Loading...</td>
-                    <td>0:00</td>
-                    <td>Loading...</td>
-                  </tr>
-
-                </tbody>
-                <tfoot>
-                  <tr>
-                    <th>Date</th>
-                    <th>Event</th>
-                    <th>Time</th>
-                    <th>Venue</th>
-                  </tr>
-                </tfoot>
-              </table>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
-
-    <!-- <div class="team-div-1">
-      <div class="container">
-        <div class="d-flex align-items-center justify-content-between">
-          <h2 class="comon-heading m-0"> Players Squad </h2>
-          <a href="#" class="btn all-cm-btn"> Show All <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-              fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-              <path fill-rule="evenodd"
-                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z">
-              </path>
-            </svg> </a>
-        </div>
-
-        <div class="team-slid owl-carousel owl-theme mt-5">
-          <a href="#" class="comon-plyaers">
-            <figure>
-              <img src="images/Yoshi_1.png" alt="team1" />
-              <figcaption>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                  class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
-                  <path
-                    d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z" />
-                </svg>
-              </figcaption>
-            </figure>
-            <div class="name d-flex align-items-center justify-content-between">
-              <h5> Ahmed Gaidam
-                <span class="d-block"> STRIKER</span>
-              </h5>
-              <span class="num"> 10 </span>
-            </div>
-          </a>
-
-          <a href="#" class="comon-plyaers">
-            <figure>
-              <img src="images/Yoshi_1.png" alt="team1" />
-              <figcaption>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                  class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
-                  <path
-                    d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z" />
-                </svg>
-              </figcaption>
-            </figure>
-            <div class="name d-flex align-items-center justify-content-between">
-              <h5> Ahmed Gaidam
-                <span class="d-block"> Defender</span>
-              </h5>
-              <span class="num"> 32 </span>
-            </div>
-          </a>
-
-          <a href="#" class="comon-plyaers">
-            <figure>
-              <img src="images/Yoshi_1.png" alt="team1" />
-              <figcaption>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                  class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
-                  <path
-                    d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z" />
-                </svg>
-              </figcaption>
-            </figure>
-            <div class="name d-flex align-items-center justify-content-between">
-              <h5> Ahmed Gaidam
-                <span class="d-block"> Forward </span>
-              </h5>
-              <span class="num"> 99 </span>
-            </div>
-          </a>
-
-          <a href="#" class="comon-plyaers">
-            <figure>
-              <img src="images/Yoshi_1.png" alt="team1" />
-              <figcaption>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                  class="bi bi-arrow-up-right-square-fill" viewBox="0 0 16 16">
-                  <path
-                    d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z" />
-                </svg>
-              </figcaption>
-            </figure>
-            <div class="name d-flex align-items-center justify-content-between">
-              <h5> Ahmed Gaidam
-                <span class="d-block"> STRIKER</span>
-              </h5>
-              <span class="num"> 15 </span>
-            </div>
-          </a>
-
-
-        </div>
-      </div>
-    </div> -->
 
     <div class="join-us-div">
       <div class="container">
---
+++ leagueSchedule.php
@@ -0,0 +1,95 @@
+<?php ?>
+
+
+<div class="table-ag-div py-5">
+    <div class="container">
+        <h2 class="comon-heading m-0"> League Table & Schedule </h2>
+
+        <div class="row g-lg-5">
+            <div class="col-lg-5">
+                <div class="table-div-left mt-4">
+                    <h4> Batch A </h4>
+
+
+                    <table id="seri1" class="display" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Club</th>
+                                <th>W</th>
+                                <th>D</th>
+                                <th>L</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+
+                                    <div class="comon-ft-cl">
+                                        <figure class="d-flex align-items-center">
+                                            <img src="images/fc-1.jpg" alt="fbn" />
+                                            <figcaption>
+                                                &nbsp;Loading...
+                                            </figcaption>
+                                        </figure>
+                                    </div>
+
+                                </td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Club</th>
+                                <th>W</th>
+                                <th>D</th>
+                                <th>L</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="table-div-left mt-4">
+                    <h4> Batch A </h4>
+
+
+                    <table id="seri2" class="display " style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Event</th>
+                                <th>Time</th>
+                                <th>Venue</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    0
+                                </td>
+                                <td> Loading.. <span>vs</span> Loading...</td>
+                                <td>0:00</td>
+                                <td>Loading...</td>
+                            </tr>
+
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Date</th>
+                                <th>Event</th>
+                                <th>Time</th>
+                                <th>Venue</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php ?>
