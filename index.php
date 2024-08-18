c72ae477e5902f99e1e03fbb007b230a218c6b6c
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-18 20:08:03 +0800

    Refactor index.php: update grid layout and add missing class to column

--- index.php
+++ index.php
@@ -948,12 +948,12 @@ require 'auth.php';
 
                         <br class="d-none d-sm-block" /><br class="d-none d-sm-block" />
 
-                        <div class="row justify-content-center">
+                        <div class="row justify-content-center row-cols-lg-auto g-3 align-items-center">
                           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                             <a type="button" href="registration.php" class="btn mt-8 w-100 join-btn fs-6"
                               style="border-radius: 15px 15px;">Register Your School</a>
                           </div>
-                          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-4">
+                          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                             <a type="button" href="referenceNumber.php" class="btn mt-8 w-100 join-btn fs-6"
                               style="border-radius: 15px 15px;">Register as a Student</a>
                           </div>
