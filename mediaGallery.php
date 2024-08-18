56788421cf0af463e207678f6a636b90c262e404
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-18 20:25:49 +0800

    Refactor yaps2024_modal.php: add modal for Yoshi Abuja Private Schools Tournaments 2024

---
+++ mediaGallery.php
@@ -0,0 +1,201 @@
+<?php ?>
+
+
+<div class="newd-blogs-div py-5">
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between">
+
+            <h2 class="comon-heading m-0"> Media Gallery </h2>
+
+            <a href="media.php" class="btn all-cm-btn animate__animated animate__zoomIn"> View All <svg
+                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z">
+                    </path>
+                </svg> </a>
+
+        </div>
+
+
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 mt-0 mt-lg-0 g-4 g-lg-4">
+
+
+            <div class="col">
+                <div class="comongallery">
+                    <a data-fancybox="gallery" href="images/Yoshi_5.jpg" class="gallery-div">
+                        <figure>
+                            <img src="images/Yoshi_5.jpg" alt="gallery1" />
+                            <figcaption>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                                </svg>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <!-- <div class="photo-details">
+                <h5>Gombe United Fc Win 2024 </h5>
+                <h6> <i class="far fa-clock"></i> 1years Ago</h6>
+              </div> -->
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="comongallery">
+                    <a data-fancybox="gallery" href="images/Yoshi_6.jpg" class="gallery-div">
+                        <figure>
+                            <img src="images/Yoshi_6.jpg" alt="gallery1" />
+                            <figcaption>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                                </svg>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <!-- <div class="photo-details">
+                <h5>Gombe United Fc Win 2024 </h5>
+                <h6> <i class="far fa-clock"></i> 1years Ago</h6>
+              </div> -->
+                </div>
+            </div>
+
+
+            <div class="col">
+                <div class="comongallery">
+                    <a data-fancybox="gallery" href="images/Yoshi_11.jpg" class="gallery-div">
+                        <figure>
+                            <img src="images/Yoshi_11.jpg" alt="gallery1" />
+                            <figcaption>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                                </svg>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <!-- <div class="photo-details">
+                <h5>Gombe United Fc Win 2024 </h5>
+                <h6> <i class="far fa-clock"></i> 1years Ago</h6>
+              </div> -->
+                </div>
+            </div>
+
+
+            <div class="col">
+                <div class="comongallery">
+                    <a data-fancybox="gallery" href="images/Yoshi_banner_2.jpg" class="gallery-div">
+                        <figure>
+                            <img src="images/Yoshi_banner_2.jpg" alt="gallery1" />
+                            <figcaption>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                                </svg>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <!-- <div class="photo-details">
+                <h5>Gombe United Fc Win 2024 </h5>
+                <h6> <i class="far fa-clock"></i> 1years Ago</h6>
+              </div> -->
+                </div>
+            </div>
+
+
+
+            <div class="col">
+                <div class="comongallery">
+                    <a data-fancybox="gallery" href="images/Yoshi_2.jpg" class="gallery-div">
+                        <figure>
+                            <img src="images/Yoshi_2.jpg" alt="gallery1" />
+                            <figcaption>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                                </svg>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <!-- <div class="photo-details">
+                <h5>Gombe United Fc Win 2024 </h5>
+                <h6> <i class="far fa-clock"></i> 1years Ago</h6>
+              </div> -->
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="comongallery">
+                    <a data-fancybox="gallery" href="images/Yoshi_7.jpg" class="gallery-div">
+                        <figure>
+                            <img src="images/Yoshi_7.jpg" alt="gallery1" />
+                            <figcaption>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                                </svg>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <!-- <div class="photo-details">
+                <h5>Gombe United Fc Win 2024 </h5>
+                <h6> <i class="far fa-clock"></i> 1years Ago</h6>
+              </div> -->
+                </div>
+            </div>
+
+
+            <div class="col">
+                <div class="comongallery">
+                    <a data-fancybox="gallery" href="images/yoshi_founder.png" class="gallery-div">
+                        <figure>
+                            <img src="images/yoshi_founder.png" alt="gallery1" />
+                            <figcaption>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                                </svg>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <!-- <div class="photo-details">
+                <h5>Gombe United Fc Win 2024 </h5>
+                <h6> <i class="far fa-clock"></i> 1years Ago</h6>
+              </div> -->
+                </div>
+            </div>
+
+
+            <div class="col">
+                <div class="comongallery">
+                    <a data-fancybox="gallery" href="images/Yoshi_9.jpg" class="gallery-div">
+                        <figure>
+                            <img src="images/Yoshi_9.jpg" alt="gallery1" />
+                            <figcaption>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                                </svg>
+                            </figcaption>
+                        </figure>
+                    </a>
+                    <!-- <div class="photo-details">
+                <h5>Gombe United Fc Win 2024 </h5>
+                <h6> <i class="far fa-clock"></i> 1years Ago</h6>
+              </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php ?>
---
+++ yaps2024_modal.php
@@ -0,0 +1,47 @@
+<?php ?>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="onloadModal" tabindex="-1" aria-labelledby="onloadModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-body d-flex align-items-center p-0 m-0">
+                <div class="row g-0 align-items-center">
+                    <div class="col-sm-6 col-xs-12">
+                        <!-- Close button for small screens -->
+                        <button type="button" data-bs-dismiss="modal"
+                            class="btn-close position-absolute top-0 end-0 m-2 d-sm-none" aria-label="Close"></button>
+                        <img src="images/yoshiStud.jpg" alt="Gift Poster" style="border-radius: 10px 10px;"
+                            class="img-fluid" id="leftImg">
+                    </div>
+                    <div class="col-sm-6 col-xs-12 text-center">
+                        <!-- Close button for large screens -->
+                        <button type="button" data-bs-dismiss="modal"
+                            class="btn-close float-end d-none d-sm-block -mt-8" aria-label="Close"></button>
+                        <div class="px-4 animated-text">
+                            <h2>Welcome to!</h2>
+                            <br class="d-none d-sm-block" />
+                            <h1>Yoshi Abuja Private Schools Tournaments 2024</h1>
+                            <p class="countdown-timer my-3" id="countdown"></p>
+                            <!-- <p class="my-3 text-secondary">Yoshi Abuja Private Schools Torunaments 2024</p>
+                        <p class="mt-3 mb-4 text-success">Football Tournament</p> -->
+                            <br class="d-none d-sm-block" /><br class="d-none d-sm-block" />
+                            <div class="row justify-content-center row-cols-lg-auto g-3 align-items-center">
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <a type="button" href="registration.php" class="btn mt-8 w-100 join-btn fs-6"
+                                        style="border-radius: 15px 15px;">Register Your School</a>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <a type="button" href="referenceNumber.php" class="btn mt-8 w-100 join-btn fs-6"
+                                        style="border-radius: 15px 15px;">Register as a Student</a>
+                                </div>
+                            </div>
+                            <br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php ?>
