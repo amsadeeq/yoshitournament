b3aaf424862d7dd8167f29845e1f80fe3355a886
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-22 00:16:50 +0800

    Refactor schoolList.php: remove unnecessary code and improve table layout and display

--- management/admin/schoolList.php
+++ management/admin/schoolList.php
@@ -394,47 +394,36 @@ $school_official = $stmt_school->fetchAll(PDO::FETCH_ASSOC);
                                 <td><?php echo $school['phone']; ?></td>
                                 <td><?php echo $school['email']; ?></td>
                               </tr>
-                              <?php
-                            }
-                            ?>
-                          </tbody>
-                        </table>
-                      </a>
-                      <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel"
-                        aria-labelledby="headingOne">
-                        <div class="panel-body">
-                          <table class="table table-bordered">
-                            <thead>
-                              <tr>
-                                <th>#</th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Username</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                              </tr>
+
                             </tbody>
                           </table>
+                        </a>
+                        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel"
+                          aria-labelledby="headingOne">
+                          <div class="panel-body">
+                            <table class="table table-bordered">
+                              <thead>
+                                <tr>
+                                  <th>#</th>
+                                  <th>First Name</th>
+                                  <th>Last Name</th>
+                                  <th>Username</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <th scope="row">1</th>
+                                  <td>Mark</td>
+                                  <td>Otto</td>
+                                  <td>@mdo</td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
                         </div>
-                      </div>
+                        <?php
+                            }
+                            ?>
                     </div>
                     <div class="panel">
                       <a class="panel-heading collapsed" role="tab" id="headingTwo" data-toggle="collapse"
