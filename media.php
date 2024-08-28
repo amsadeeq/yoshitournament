b9eab6ad340736b8357e88877f981dcbc1331e0a
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-28 18:42:01 +0800

    Refactor media.php: add lazy loading for gallery image

--- media.php
+++ media.php
@@ -357,6 +357,27 @@ require 'auth.php';
               </div>
             </div>
 
+            <div class="col">
+              <div class="comongallery">
+                <a data-fancybox="gallery" href="images/abuja_ (178).JPG" class="gallery-div">
+                  <figure>
+                    <img src="images/abuja_ (178).JPG" alt="gallery1" />
+                    <figcaption>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-plus-lg" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                          d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                      </svg>
+                    </figcaption>
+                  </figure>
+                </a>
+                <!-- <div class="photo-details">
+                  <h5>Gombe United Fc Win 2024 </h5>
+                  <h6> <i class="far fa-clock"></i> 1years Ago</h6>
+                </div> -->
+              </div>
+            </div>
+
 
 
 
