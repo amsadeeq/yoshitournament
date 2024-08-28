430d11b6a7a96d4337bfc320de33e2bafdceeaf1
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-29 05:44:30 +0800

    Refactor attendance.php: optimize QR code scanning and improve image rendering

--- management/admin/attendance.php
+++ management/admin/attendance.php
@@ -415,38 +415,38 @@ $attendances = $stmt->fetchAll(PDO::FETCH_ASSOC);
     let scanner;
 
     function startScanner() {
-      scanner = new Instascan.Scanner({ video: document.getElementById('interactive'), mirror: true });
+      scanner = new Instascan.Scanner({ video: document.getElementById('interactive'), mirror: false });
 
       scanner.addListener('scan', function (content) {
-      $("#detected-qr-code").val(content);
-      console.log(content);
-      scanner.stop();
-      document.querySelector(".qr-detected-container").style.display = '';
-      document.querySelector(".scanner-con").style.display = 'none';
+        $("#detected-qr-code").val(content);
+        console.log(content);
+        scanner.stop();
+        document.querySelector(".qr-detected-container").style.display = '';
+        document.querySelector(".scanner-con").style.display = 'none';
       });
 
       Instascan.Camera.getCameras()
-      .then(function (cameras) {
-        if (cameras.length > 0) {
-        // Use the back camera if available, otherwise use the front camera
-        const selectedCamera = cameras.find(camera => camera.name.includes('back')) || cameras[0];
-        scanner.start(selectedCamera);
-        } else {
-        console.error('No cameras found.');
-        alert('No cameras found.');
-        }
-      })
-      .catch(function (err) {
-        console.error('Camera access error:', err);
-        alert('Camera access error: ' + err);
-      });
+        .then(function (cameras) {
+          if (cameras.length > 0) {
+            // Use the back camera if available, otherwise use the front camera
+            const selectedCamera = cameras.find(camera => camera.name.includes('back')) || cameras[0];
+            scanner.start(selectedCamera);
+          } else {
+            console.error('No cameras found.');
+            alert('No cameras found.');
+          }
+        })
+        .catch(function (err) {
+          console.error('Camera access error:', err);
+          alert('Camera access error: ' + err);
+        });
     }
 
     document.addEventListener('DOMContentLoaded', startScanner);
 
     function deleteAttendance(id) {
       if (confirm("Do you want to remove this attendance?")) {
-      window.location = "./endpoint/delete-attendance.php?attendance=" + id;
+        window.location = "./endpoint/delete-attendance.php?attendance=" + id;
       }
     }
   </script>
