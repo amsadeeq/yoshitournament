b1139a678233a74e87cf6d5fa85c61a604865e97
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-29 04:41:31 +0800

    Refactor attendance.php: optimize attendance list rendering

--- management/admin/validate_attendance.php
+++ management/admin/validate_attendance.php
@@ -16,51 +16,50 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt->execute(['userRefNo' => $qrCode]);
         $selectStmt = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        echo $selectStmt['surname'];
-
-        // // $selectStmt = $conn->prepare("SELECT tbl_student_id FROM tbl_student WHERE generated_code = :generated_code");
-        // // $selectStmt->bindParam(":generated_code", $qrCode, PDO::PARAM_STR);
-
-        // if ($selectStmt->execute()) {
-        //     $result = $selectStmt->fetch();
-        //     if ($result !== false) {
-        //         $userRefNo = $result["userRefNo"];
-        //         $timeIn = date("Y-m-d H:i:s");
-        //         // Update attendance details
-        //         $attendance_time = date("H:i:s");
-        //         $attendance_date = date("Y-m-d");
-        //     } else {
-        //         echo "No student found in QR Code";
-        //     }
-        // } else {
-        //     echo "Failed to execute the statement.";
-        // }
 
 
-        // try {
-        //     $updateStmt = $pdo->prepare("UPDATE yoshi_school_students_tbl SET attendance = 1, attendance_time = :attendance_time,
-        //     attendance_date = :attendance_date WHERE userRefNo = :userRefNo");
-        //     $updateStmt->execute([
-        //         'attendance_time' => $attendance_time,
-        //         'attendance_date' => $attendance_date,
-        //         'userRefNo' => $userRefNo
-        //     ]);
 
-        //     header("Location: attendance.php");
 
-        //     exit();
-        // } catch (PDOException $e) {
-        //     echo "Error:" . $e->getMessage();
-        // }
 
-    } //else {
-    //     echo "
-    //         <script>
-    //             alert('Please fill in all fields!');
-    //             window.location.href = 'attendance.php';
-    //         </script>
-    //     ";
-    // }
+
+        if ($selectStmt !== false) {
+            $userRefNo = $selectStmt["userRefNo"];
+            //$timeIn = date("Y-m-d H:i:s");
+            // Update attendance details
+            $attendance_time = date("H:i:s");
+            $attendance_date = date("Y-m-d");
+        } else {
+            echo "<script>
+            alert('User reference number not found.');
+            </script>";
+        }
+
+
+
+        try {
+            $updateStmt = $pdo->prepare("UPDATE yoshi_school_students_tbl SET attendance = 1, attendance_time = :attendance_time,
+            attendance_date = :attendance_date WHERE userRefNo = :userRefNo");
+            $updateStmt->execute([
+                'attendance_time' => $attendance_time,
+                'attendance_date' => $attendance_date,
+                'userRefNo' => $userRefNo
+            ]);
+
+            header("Location: attendance.php");
+
+            exit();
+        } catch (PDOException $e) {
+            echo "Error:" . $e->getMessage();
+        }
+
+    } else {
+        echo "
+            <script>
+                alert('Please fill in all fields!');
+                window.location.href = 'attendance.php';
+            </script>
+        ";
+    }
 }
 
 
