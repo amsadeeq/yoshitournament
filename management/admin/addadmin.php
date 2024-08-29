05e67175c62d7a3d88bf3c70539521908f5e5363
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-08-30 01:03:22 +0800

    Refactor addadmin.php: update admin form with gender selection and phone number field

--- management/admin/addadmin.php
+++ management/admin/addadmin.php
@@ -346,8 +346,8 @@ $email = $_SESSION['a_email'];
 												class="col-form-label col-md-3 col-sm-3 label-align">Phone
 												Number</label>
 											<div class="col-md-6 col-sm-6 ">
-												<input id="middle-name" class="form-control" type="text"
-													name="middle-name">
+												<input id="middle-name" id="last-name" class="form-control" type="text"
+													name="phone">
 											</div>
 										</div>
 										<div class="item form-group">
@@ -374,48 +374,12 @@ $email = $_SESSION['a_email'];
 											</div>
 										</div>
 
-										<div class="item form-group">
-											<label class="col-form-label col-md-3 col-sm-3 label-align">Gender</label>
-											<div class="col-md-6 col-sm-6 ">
-												<div id="gender" class="btn-group" data-toggle="buttons">
-													<label class="btn btn-secondary" data-toggle-class="btn-primary"
-														data-toggle-passive-class="btn-default">
-														<input type="radio" name="gender" value="male" class="join-btn">
-														&nbsp; Male &nbsp;
-													</label>
-													<label class="btn btn-primary" data-toggle-class="btn-primary"
-														data-toggle-passive-class="btn-default">
-														<input type="radio" name="gender" value="female"
-															class="join-btn"> Female
-													</label>
-												</div>
-											</div>
-										</div>
-										<div class="item form-group">
-											<label class="col-form-label col-md-3 col-sm-3 label-align">Date Of Birth
-												<span class="required">*</span>
-											</label>
-											<div class="col-md-6 col-sm-6 ">
-												<input id="birthday" class="date-picker form-control"
-													placeholder="dd-mm-yyyy" type="text" required="required" type="text"
-													onfocus="this.type='date'" onmouseover="this.type='date'"
-													onclick="this.type='date'" onblur="this.type='text'"
-													onmouseout="timeFunctionLong(this)">
-												<script>
-													function timeFunctionLong(input) {
-														setTimeout(function () {
-															input.type = 'text';
-														}, 60000);
-													}
-												</script>
-											</div>
-										</div>
 										<div class="ln_solid"></div>
 										<div class="item form-group">
 											<div class="col-md-6 col-sm-6 offset-md-3">
 												<button class="btn btn-primary" type="button">Cancel</button>
-												<button class="btn btn-primary" type="reset">Reset</button>
-												<button type="submit" class="btn btn-success">Submit</button>
+
+												<button type="submit" class="btn btn-success">Add admin</button>
 											</div>
 										</div>
 
