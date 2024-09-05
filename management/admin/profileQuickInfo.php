517ca85e05541bfb39758c9e9d3cb3fb3573663c
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-09-05 20:15:32 +0800

    Refactor profileQuickInfo.php and smallNavbar.php: Update welcome message and branding

--- management/admin/profileQuickInfo.php
+++ management/admin/profileQuickInfo.php
@@ -5,7 +5,7 @@
     </div>
     <div class="profile_info">
         <span>Welcome,</span>
-        <h2>John Doe</h2>
+        <h2>Admin</h2>
     </div>
 </div>
 <?php ?>
--- management/admin/smallNavbar.php
+++ management/admin/smallNavbar.php
@@ -2,7 +2,7 @@
 
 <div class="navbar nav_title" style="border: 0;">
     <a href="mainDashboard.php" class="site_title"><img src="../../images/logo.png"
-            style="width: 40px; height: 40px;" /> <span>Yoshi Tournaments</span></a>
+            style="width: 40px; height: 40px;" /> <span>Yoshi Admin</span></a>
 </div>
 
 <?php ?>
