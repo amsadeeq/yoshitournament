81b3e8f3026ef74586fd03095617687cf391adc4
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-09-05 18:46:58 +0800

    Refactor topNavbar.php: Update navbar styling and user profile dropdown

--- management/admin/topNavbar.php
+++ management/admin/topNavbar.php
@@ -6,9 +6,10 @@
             <a id="menu_toggle"><i class="fa fa-bars"></i></a>
         </div>
         <nav class="nav navbar-nav">
-            <ul class=" navbar-right">
-                <li class="nav-item dropdown open" style="padding-left: 15px; border-radius:15px 15px !important;">
-                    <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown"
+            <ul class=" navbar-right" style="border-radius:15px 15px !important;">
+                <li class="nav-item dropdown open" style="padding-left: 15px;">
+                    <a href="javascript:;" style="border-radius:15px 15px !important;"
+                        class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown"
                         data-toggle="dropdown" aria-expanded="false">
                         <img src="images/img.jpg" alt="">Admin
                     </a>
