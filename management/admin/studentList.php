0e41ef9bc4b44bcfc5a7c695822a6150404754e1
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-09-05 21:01:38 +0800

    Refactor studentList.php: Remove unnecessary page title, search functionality, and unused code

--- management/admin/studentList.php
+++ management/admin/studentList.php
@@ -74,31 +74,12 @@
               <div class="x_panel">
                 <div class="x_title">
                   <h2>Student List </h2>
-                  <ul class="nav navbar-right panel_toolbox">
-                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                    </li>
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i
-                          class="fa fa-wrench"></i></a>
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">Settings 1</a>
-                        <a class="dropdown-item" href="#">Settings 2</a>
-                      </div>
-                    </li>
-                    <li><a class="close-link"><i class="fa fa-close"></i></a>
-                    </li>
-                  </ul>
                   <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
                   <div class="row">
                     <div class="col-sm-12">
                       <div class="card-box table-responsive">
-                        <p class="text-muted font-13 m-b-30">
-                          The Buttons extension for DataTables provides a common set of options, API methods and styling
-                          to display buttons on a page that will interact with a DataTable. The core library provides
-                          the based framework upon which plug-ins can built.
-                        </p>
                         <table id="datatable-buttons" class="table table-striped table-bordered" style="width:100%">
                           <thead>
                             <tr>
@@ -586,32 +567,12 @@
               <div class="x_panel">
                 <div class="x_title">
                   <h2>Responsive example<small>Users</small></h2>
-                  <ul class="nav navbar-right panel_toolbox">
-                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                    </li>
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i
-                          class="fa fa-wrench"></i></a>
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">Settings 1</a>
-                        <a class="dropdown-item" href="#">Settings 2</a>
-                      </div>
-                    </li>
-                    <li><a class="close-link"><i class="fa fa-close"></i></a>
-                    </li>
-                  </ul>
                   <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
                   <div class="row">
                     <div class="col-sm-12">
                       <div class="card-box table-responsive">
-                        <p class="text-muted font-13 m-b-30">
-                          Responsive is an extension for DataTables that resolves that problem by optimising the table's
-                          layout for different screen sizes through the dynamic insertion and removal of columns from
-                          the table.
-                        </p>
-
                         <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap"
                           cellspacing="0" width="100%">
                           <thead>
