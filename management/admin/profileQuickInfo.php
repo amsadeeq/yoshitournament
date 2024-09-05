9d351a95b86eea05a2ad0894a4107b4a9ef8a6d5
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-09-05 17:59:01 +0800

    Refactor admin registration form: update admin's password when temporary password matches

---
+++ management/admin/profileQuickInfo.php
@@ -0,0 +1,12 @@
+<?php ?>
+<div class="profile clearfix">
+    <div class="profile_pic">
+        <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+
+    </div>
+    <div class="profile_info">
+        <span>Welcome,</span>
+        <h2>Abubakar Sadiq</h2>
+    </div>
+</div>
+<?php ?>
---
+++ management/admin/smallNavbar.php
@@ -0,0 +1,8 @@
+<?php ?>
+
+<div class="navbar nav_title" style="border: 0;">
+    <a href="mainDashboard.php" class="site_title"><img src="../../images/logo.png"
+            style="width: 40px; height: 40px;" /> <span>Yoshi Tournaments</span></a>
+</div>
+
+<?php ?>
