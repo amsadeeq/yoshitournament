dfdf75c52e69886f6016315c11faad9ead440686
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-09-13 00:15:40 +0800

    Refactor news.php: Add news item for Yoshi-Dubai-Abuja Primary, Secondary Schools Football Tournament

--- news.php
+++ news.php
@@ -183,6 +183,45 @@ require 'auth.php';
             </a>
           </div>
 
+          <div class="col">
+            <a href="https://bsnsports.com.ng/post/Yoshi-Dubai-Abuja-Primary--Secondary-Schools-Football-Tournament-Kicks-off-September-14"
+              class="comon-blogs1">
+              <figure>
+                <img src="images/YAPS_2024_KICKOFF.jpg" alt="bn" />
+                <figcaption>
+                  <b class="d-block">12</b>
+                  Sep
+                </figcaption>
+              </figure>
+              <div class="news-items-del">
+                <h5> Yoshi-Dubai-Abuja Primary, Secondary Schools Football Tournament Kicks off September 14</h5>
+                <ul class="list-unstyled d-flex align-items-center my-2">
+                  <li>
+                    <i class="far fa-user"></i> Abubakar
+                  </li>
+                  <li>
+                    <i class="far fa-comment"></i> 0
+                  </li>
+                  <li>
+                    <i class="far fa-clock"></i> 1 Day ago
+                  </li>
+                </ul>
+
+                <p class="mt-2" style="text-align: justify !important;"> Yoshi-Dubai-Abuja football tournament for
+                  Primary, Junior Secondary and Senior Secondary schools will kick off on 14th September, 2024 at the
+                  Astro Turf, Accra street, Wuse Zone 5, Abuja.
+                </p>
+
+                <h6 class="btn reda-btn mt-3"> Read more <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                      d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z" />
+                  </svg> </h6>
+              </div>
+
+            </a>
+          </div>
+
         </div>
 
 
