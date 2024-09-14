3517f9a389c75e00d6b519bd09f156fa5350316e
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-09-15 01:58:03 +0800

    Refactor header navigation in schedule.php

--- header.php
+++ header.php
@@ -1,87 +1,85 @@
-<?php
-
-echo '
+<?php ?>
 
 <header class="float-start w-100">
 
-<nav class="navbar navbar-expand-lg navbar-light ">
-  <div class="container">
-    <a class="navbar-brand" href="index.php">
-      <img src="images/logo.png" alt="logo" class="yoshi_logo" />
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRightmobile">
-      <i class="fas fa-bars"></i>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+  <nav class="navbar navbar-expand-lg navbar-light ">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">
+        <img src="images/logo.png" alt="logo" class="yoshi_logo" />
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRightmobile">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
-        <li class="nav-item">
-          <a class="nav-link" href="index.php">Home</a>
-        </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
 
 
 
-        <li class="nav-item">
-          <a class="nav-link" href="tournaments.php">Tournament</a>
-        </li>
+          <li class="nav-item">
+            <a class="nav-link" href="tournaments.php">Tournament</a>
+          </li>
 
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="matches.php">Events</a>
-        </li> -->
+          <li class="nav-item">
+            <a class="nav-link" href="matches.php">Matches</a>
+          </li>
 
-        <li class="nav-item">
-          <a class="nav-link" href="registration.php">Registration</a>
-        </li>
+          <li class="nav-item">
+            <a class="nav-link" href="registration.php">Registration</a>
+          </li>
 
-        <!-- <li class="nav-item">
+          <!-- <li class="nav-item">
       <a class="nav-link " href="schedule.php">Schedule</a>
     </li> -->
 
-        <li class="nav-item">
-          <a class="nav-link " href="news.php">News</a>
-        </li>
+          <li class="nav-item">
+            <a class="nav-link " href="news.php">News</a>
+          </li>
 
-        <!-- <li class="nav-item">
+          <!-- <li class="nav-item">
       <a class="nav-link " href="players.php">Players</a>
     </li> -->
 
-        <li class="nav-item">
-          <a class="nav-link " href="media.php">Media</a>
-        </li>
+          <li class="nav-item">
+            <a class="nav-link " href="media.php">Media</a>
+          </li>
 
-        <li class="nav-item">
-          <a class="nav-link " href="about.php">About us</a>
-        </li>
-        <!-- <li class="nav-item">
+          <li class="nav-item">
+            <a class="nav-link " href="about.php">About us</a>
+          </li>
+          <!-- <li class="nav-item">
         <a class="nav-link " href="shop.php">Shop</a>
       </li> -->
 
-        <li class="nav-item">
-          <a class="nav-link " href="contact.php">Contact</a>
-        </li>
+          <li class="nav-item">
+            <a class="nav-link " href="contact.php">Contact</a>
+          </li>
 
-        <li class="nav-item">
-          <a class="nav-link btn join-btn animate__animated animate__zoomIn" data-bs-toggle="modal"
-            class="regster-bn" data-bs-target="#loginModal">
+          <li class="nav-item">
+            <a class="nav-link btn join-btn animate__animated animate__zoomIn" data-bs-toggle="modal" class="regster-bn"
+              data-bs-target="#loginModal">
 
-            Login</a>
-        </li>
+              Login</a>
+          </li>
 
 
-        <li class="nav-item">
-          <a class="nav-link btn bar-btn" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRightmobile"><i
-              class="fas fa-bars"></i></a>
-        </li>
+          <li class="nav-item">
+            <a class="nav-link btn bar-btn" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRightmobile"><i
+                class="fas fa-bars"></i></a>
+          </li>
 
-      </ul>
+        </ul>
 
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
 </header>
-';
-//<li class="nav-item">
-//<a class="nav-link active" href="index.php">Home</a>
-//</li>
-?>
+
+<!-- <li class="nav-item">
+  <a class="nav-link active" href="index.php">Home</a>
+</li> -->
+<?php ?>
---
+++ userIcons.php
@@ -0,0 +1,28 @@
+<?php ?>
+
+<div class="cart-sec-ban">
+    <ul class="list-unstyled">
+        <li>
+            <a href="#" class="btn side_btn" data-bs-toggle="modal" class="regster-bn" data-bs-target="#loginModal">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person"
+                    viewBox="0 0 16 16">
+
+                    <path
+                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                </svg>
+
+            </a>
+        </li>
+        <li>
+            <a href="matches.php" class="btn side_btn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z" />
+                </svg>
+            </a>
+        </li>
+    </ul>
+</div>
+
+<?php ?>
