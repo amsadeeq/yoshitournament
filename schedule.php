c954d51b0de60c1c8fd7939bde29e7b86ac657ce
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-09-15 02:04:49 +0800

    Refactor header navigation in schedule.php and remove commented out code

--- schedule.php
+++ schedule.php
@@ -83,7 +83,7 @@ require 'auth.php';
                 <th>League</th>
                 <th> Season </th>
                 <th>Venue</th>
-                <th class="last">Action</th>
+                <!-- <th class="last">Action</th> -->
               </tr>
             </thead>
             <tbody>
@@ -110,7 +110,7 @@ require 'auth.php';
                 <td>Primary</td>
                 <td>2024</td>
                 <td>Abuja Stadium</td>
-                <td>
+                <!-- <td>
                   <a href="booking.php" class="btn details-btn"> buy ticket <svg xmlns="http://www.w3.org/2000/svg"
                       width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right-square-fill"
                       viewBox="0 0 16 16">
@@ -118,7 +118,7 @@ require 'auth.php';
                         d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707z">
                       </path>
                     </svg> </a>
-                </td>
+                </td> -->
               </tr>
 
 
