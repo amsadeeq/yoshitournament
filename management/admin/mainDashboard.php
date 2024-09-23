3e44ec9f38ef9d1c9f060aba513b7d9451c4f0b9
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-09-23 20:41:02 +0800

    Refactor mainDashboard.php: Update administration section

--- management/admin/mainDashboard.php
+++ management/admin/mainDashboard.php
@@ -356,27 +356,12 @@ $total_student = $result_student['total_student'];
 
         <div class="x_panel">
           <div class="x_title">
-            <h2>Button App Design</h2>
-            <ul class="nav navbar-right panel_toolbox">
-              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-              </li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i
-                    class="fa fa-wrench"></i></a>
-                <ul class="dropdown-menu" role="menu">
-                  <li><a href="#">Settings 1</a>
-                  </li>
-                  <li><a href="#">Settings 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li><a class="close-link"><i class="fa fa-close"></i></a>
-              </li>
-            </ul>
+            <h2>Administation</h2>
+
             <div class="clearfix"></div>
           </div>
           <div class="x_content">
-            <p>Add the class <code>.btn .btn-app</code> tag</p>
+            <!-- <p>Add the class <code>.btn .btn-app</code> tag</p> -->
             <a class="btn btn-app">
               <i class="fa fa-edit"></i> Edit
             </a>
