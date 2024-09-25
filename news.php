612e6aebe03964f1eb03a54d62ed8592a9a243a8
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2024-09-26 00:53:55 +0800

    Refactor featuresNews.php: Update image source for news cover picture and modify code to fetch featured news from the database

--- news.php
+++ news.php
@@ -119,7 +119,47 @@ try {
           <?php
           foreach ($featured_news as $news) {
             $newsTitle = $news['title'];
-            echo $newsTitle;
+
+            ?>
+
+            <div class="col">
+              <a href="<?php echo $news['news_url']; ?>" class="comon-blogs1">
+                <figure>
+                  <img src="management/admin/featured_news_photo/<?php echo $news['cover_picture_name']; ?>" alt="bn" />
+                  <figcaption>
+                    <b class="d-block">21</b>
+                    Sep
+                  </figcaption>
+                </figure>
+                <div class="news-items-del">
+                  <h5><?php echo $news['title']; ?></h5>
+                  <ul class="list-unstyled d-flex align-items-center my-2">
+                    <li>
+                      <i class="far fa-user"></i> <?php echo $news['author']; ?>
+                    </li>
+                    <li>
+                      <i class="far fa-comment"></i> 125
+                    </li>
+                    <li>
+                      <i class="far fa-clock"></i> 1 Day ago
+                    </li>
+                  </ul>
+
+                  <p class="mt-2" style="text-align: justify !important;"> <?php echo $news['short_higlight']; ?>.</p>
+
+                  <h6 class="btn reda-btn mt-3"> Read more <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                      fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd"
+                        d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z" />
+                    </svg> </h6>
+                </div>
+
+              </a>
+            </div>
+
+
+            <?php
+
           }
           ?>
           <div class="col">
